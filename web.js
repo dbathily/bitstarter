25c3de9d48f1d41fe38180abb004303621209fee
Didier Bathily <didier.bathily@njin.fr>
2013-07-07 15:58:49 +0200

    Read from index.html
    Signed-off-by: Didier Bathily <didier.bathily@njin.fr>

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var message = fs.readFileAsync("index.html").toString('utf-8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(message);
 });
 
 var port = process.env.PORT || 5000;
