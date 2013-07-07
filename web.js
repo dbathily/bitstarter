06cedb8a4a780761bcd91baf62c443fb4a4be498
Didier Bathily <didier.bathily@njin.fr>
2013-07-07 16:00:54 +0200

    Read from index.html
    Signed-off-by: Didier Bathily <didier.bathily@njin.fr>

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var message = fs.readFileAsync("index.html").toString('utf-8');
+var message = fs.readFileSync("index.html").toString('utf-8');
 
 app.get('/', function(request, response) {
   response.send(message);
